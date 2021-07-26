56b6fe1753f6934dc2b7b5f237896ed580d2bd98
Robert Stepanek <rsto@fastmailteam.com>
2021-07-26 15:38:44 +0200

    Add library for guessing IANA timezone ids from VTIMEZONE

---
+++ guesstz/tests/test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+guesstzdb="$srcdir/../zoneinfo/guesstz.db"
+val=`echo "$vtz" | sed 's/$/\r/g'  | ./cyr_guesstz -r $trstart/$trend $guesstzdb`
+if [ "$val" = "$want" ]; then
+    exit 0
+else
+    exit 1
+fi
